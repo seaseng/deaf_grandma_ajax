488a73e41448740fb5e58ce5342b6a7b2e3cb285
Navid Mostafavi <navidm@gmail.com>
2013-06-25 15:04:40 -0700

    grandma working

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,18 +1,38 @@
-get '/' do
-  if params[:user_input] == params[:user_input].upcase
-    @grandma = "Yea, I heard ya!!!!"
-  else
-    @grandma = "Speak up asshole... :( "
-  end
+# get '/' do
+#   if params[:user_input] == params[:user_input].upcase
+#     @grandma = "Yea, I heard ya!!!!"
+#   else
+#     @grandma = "Speak up asshole... :( "
+#   end
 
-  # @grandma = params[:user_input]
+#   # @grandma = params[:user_input]
+#   # Look in app/views/index.erb
+#   erb :index
+# end
+
+# post '/grandma' do
+#   # "#{params[:user_input]}"
+#   # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+#   redirect to("/?user_input=#{params[:user_input]}")
+#   # redirect to("/")
+# end
+
+get '/' do
+  @grandma = params[:grandma]
   # Look in app/views/index.erb
   erb :index
 end
 
 post '/grandma' do
-  # "#{params[:user_input]}"
-  # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
-  redirect to("/?user_input=#{params[:user_input]}")
-  # redirect to("/")
+  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  if params[:user_input] == params[:user_input].upcase
+    @grandma = 'Yea, I heard ya!!!!'
+  else
+    @grandma = 'Speak up asshole... :('
+  end
+
+  erb :index
+
+  # redirect to("/?grandma=#{@grandma}")
+
 end
